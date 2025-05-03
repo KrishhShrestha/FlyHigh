cc5e70f14b53e33a805329f048a8c44dbccb48eb
Abhijit-Mahato59 <abhi.abhijit2059@gmail.com>
2025-05-04 02:17:37 +0545

    Cart table changed

--- flyhigh.sql
+++ flyhigh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2025 at 10:05 AM
+-- Generation Time: May 03, 2025 at 10:28 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,7 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cart_item` (
-  `Cart_item_id` int(11) NOT NULL,
+  `User_id` int(11) NOT NULL,
+  `Drone_id` int(11) NOT NULL,
+  `Cart_item_id` int(10) NOT NULL,
   `Cart_item_quantity` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -52,14 +54,13 @@ CREATE TABLE `category` (
 CREATE TABLE `drone` (
   `Drone_id` int(11) NOT NULL,
   `Drone_name` varchar(255) NOT NULL,
-  `Drone_description` varchar(255) DEFAULT NULL,
-  `Drone_price` int(50) NOT NULL,
-  `Drone_weight` float NOT NULL,
-  `Flight_time` float NOT NULL,
-  `Drone_range` float NOT NULL,
+  `Drone_description` varchar(255) NOT NULL,
+  `Drone_price` double NOT NULL,
+  `Weight_grams` float DEFAULT NULL,
+  `Flight_time_minutes` float DEFAULT NULL,
+  `Range_meter` float DEFAULT NULL,
   `Camera_quality` varchar(50) DEFAULT NULL,
-  `Drone_quantity` int(10) NOT NULL DEFAULT 0,
-  `Drone_dimension` varchar(20) NOT NULL,
+  `Dimension` varchar(20) DEFAULT NULL,
   `Drone_image` varchar(255) DEFAULT NULL,
   `Category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -108,18 +109,6 @@ INSERT INTO `user` (`User_id`, `User_name`, `User_phone`, `Gender`, `User_DOB`, 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_drone_cart_item`
---
-
-CREATE TABLE `user_drone_cart_item` (
-  `User_id` int(11) NOT NULL,
-  `Drone_id` int(11) NOT NULL,
-  `Cart_item_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user_drone_order`
 --
 
@@ -137,7 +126,9 @@ CREATE TABLE `user_drone_order` (
 -- Indexes for table `cart_item`
 --
 ALTER TABLE `cart_item`
-  ADD PRIMARY KEY (`Cart_item_id`);
+  ADD PRIMARY KEY (`Cart_item_id`),
+  ADD KEY `user_user_category_drone_cart_item_fk` (`User_id`),
+  ADD KEY `drone_user_category_drone_cart_item_fk` (`Drone_id`);
 
 --
 -- Indexes for table `category`
@@ -151,7 +142,6 @@ ALTER TABLE `category`
 --
 ALTER TABLE `drone`
   ADD PRIMARY KEY (`Drone_id`),
-  ADD UNIQUE KEY `unique_drone_name` (`Drone_name`),
   ADD KEY `category_drone_fk` (`Category_id`);
 
 --
@@ -169,14 +159,6 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `unique_user_phone` (`User_phone`);
 
 --
--- Indexes for table `user_drone_cart_item`
---
-ALTER TABLE `user_drone_cart_item`
-  ADD KEY `user_user_category_drone_cart_item_fk` (`User_id`),
-  ADD KEY `drone_user_category_drone_cart_item_fk` (`Drone_id`),
-  ADD KEY `cart_item_user_category_drone_cart_item_fk` (`Cart_item_id`);
-
---
 -- Indexes for table `user_drone_order`
 --
 ALTER TABLE `user_drone_order`
@@ -187,12 +169,6 @@ ALTER TABLE `user_drone_order`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `cart_item`
---
-ALTER TABLE `cart_item`
-  MODIFY `Cart_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -223,18 +199,17 @@ ALTER TABLE `user`
 --
 
 --
+-- Constraints for table `cart_item`
+--
+ALTER TABLE `cart_item`
+  ADD CONSTRAINT `drone_user_category_drone_cart_item_fk` FOREIGN KEY (`Drone_id`) REFERENCES `drone` (`Drone_id`),
+  ADD CONSTRAINT `user_user_category_drone_cart_item_fk` FOREIGN KEY (`User_id`) REFERENCES `user` (`User_id`);
+
+--
 -- Constraints for table `drone`
 --
 ALTER TABLE `drone`
   ADD CONSTRAINT `category_drone_fk` FOREIGN KEY (`Category_id`) REFERENCES `category` (`Category_id`);
-
---
--- Constraints for table `user_drone_cart_item`
---
-ALTER TABLE `user_drone_cart_item`
-  ADD CONSTRAINT `cart_item_user_category_drone_cart_item_fk` FOREIGN KEY (`Cart_item_id`) REFERENCES `cart_item` (`Cart_item_id`),
-  ADD CONSTRAINT `drone_user_category_drone_cart_item_fk` FOREIGN KEY (`Drone_id`) REFERENCES `drone` (`Drone_id`),
-  ADD CONSTRAINT `user_user_category_drone_cart_item_fk` FOREIGN KEY (`User_id`) REFERENCES `user` (`User_id`);
 
 --
 -- Constraints for table `user_drone_order`
